0593c0b4f9ad4310541b5f2f887baa4e374e562d
Aneesh Agarwal <aganeesh@gmail.com>
2021-03-11 14:42:15 -0500

    setup database model, add fixme and todo

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,30 +6,37 @@ import logging
 import unittest
 import os
 from service.models import InventoryModel, DataValidationError, db
+from service import app
 
+DATABASE_URI = os.getenv(
+    "DATABASE_URI", "postgres://postgres:postgres@localhost:5432/postgres"
+)
 ######################################################################
-#  <your resource name>   M O D E L   T E S T   C A S E S
+#  I N V E N T O R I E S   M O D E L   T E S T   C A S E S
 ######################################################################
 class TestInventoryModel(unittest.TestCase):
     """ Test Cases for InventoryModel Model """
 
     @classmethod
     def setUpClass(cls):
-        """ This runs once before the entire test suite """
-        pass
+        """ These run once per Test suite """
+        app.config['TESTING'] = True
+        app.config['DEBUG'] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        InventoryModel.init_db(app)
 
     @classmethod
     def tearDownClass(cls):
-        """ This runs once after the entire test suite """
         pass
 
     def setUp(self):
-        """ This runs before each test """
-        pass
+        db.drop_all()  # clean up the last tests
+        db.create_all()  # make our sqlalchemy tables
 
     def tearDown(self):
-        """ This runs after each test """
-        pass
+        db.session.remove()
+        db.drop_all()
 
     ######################################################################
     #  T E S T   C A S E S
