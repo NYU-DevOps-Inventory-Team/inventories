872377ed5a15928f98ae175032b8959fec19a778
Neha Tummala <nt2194@stern.nyu.edu>
2021-03-13 16:47:03 -0500

    Copied in Template for Delete PET

--- service/routes.py
+++ service/routes.py
@@ -31,6 +31,23 @@ def index():
 
 
 ######################################################################
+# DELETE A PET
+######################################################################
+@app.route("/pets/<int:pet_id>", methods=["DELETE"])
+def delete_product_in_inventory(product_in_inventory_id):
+    """
+    Delete a Product in Inventory
+    This endpoint will delete a Product in Inventory based the id specified in the path
+    """
+    app.logger.info("Request to delete pet with id: %s", product_in_inventory_id)
+    product_in_inventory = Pet.find(product_in_inventory_id)
+    if product_in_inventory:
+        product_in_inventory.delete()
+    return make_response("", status.HTTP_204_NO_CONTENT)
+
+
+
+######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
 
@@ -39,3 +56,4 @@ def init_db():
     """ Initialies the SQLAlchemy app """
     global app
     InventoryModel.init_db(app)
+
