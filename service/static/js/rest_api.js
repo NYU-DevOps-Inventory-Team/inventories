8cd0f5435a522b14f412770cc65c378258aecded
Julie Rosenbaum <jar892@nyu.edu>
2021-04-18 15:37:37 -0400

    move /static to /service; rearrage and cleanup JS code smells

--- service/static/js/rest_api.js
+++ service/static/js/rest_api.js
@@ -9,18 +9,17 @@ $(function () {
         $("#inventory_id").val(res._id);
         $("#product_id").val(res.product_id);
         $("#product_name").val(res.product_name);
-        $("#quantity").val(res.quantity);
         $("#supplier_id").val(res.supplier_id);
         $("#supplier_name").val(res.supplier_name);
-        $("#unit_price").val(res.unit_price);
-        $("#restock_threshold").val(res.restock_threshold);
-
-
         if (res.supplier_status === "enabled") {
             $("#supplier_status").val("enabled");
         } else {
             $("#supplier_status").val("disabled");
         }
+        $("#quantity").val(res.quantity);
+        $("#unit_price").val(res.unit_price);
+        $("#restock_threshold").val(res.restock_threshold);
+
     }
 
     /// Clears all form fields
@@ -28,10 +27,10 @@ $(function () {
         $("#inventory_id").val("");
         $("#product_id").val("");
         $("#product_name").val("");
-        $("#quantity").val("");
-        $("#supplier_status").val("");
-        $("#supplier_name").val("");
         $("#supplier_id").val("");
+        $("#supplier_name").val("");
+        $("#supplier_status").val("");
+        $("#quantity").val("");
         $("#unit_price").val("");
         $("#restock_threshold").val("");
     }
@@ -47,42 +46,41 @@ $(function () {
     // ****************************************
 
     $("#create-btn").click(function () {
+        const inventory_id = $("#inventory_id").val();
+        const product_id = $("#product_id").val();
+        const product_name = $("#product_name").val();
+        const supplier_id = $("#supplier_id").val();
+        const supplier_name = $("#supplier_name").val();
+        const supplier_status = $("#supplier_status").val() === "enabled";
+        const quantity = $("#quantity").val();
+        const unit_price = $("#unit_price").val();
+        const restock_threshold = $("#restock_threshold").val();
 
-        var product_name = $("#product_name").val();
-        var supplier_name = $("#supplier_name").val();
-        var supplier_id = $("#supplier_id").val();
-        var product_id = $("#product_id").val();
-        var inventory_id = $("#inventory_id").val();
-        var quantity = $("#quantity").val();
-        var unit_price = $("#unit_price").val();
-        var restock_threshold = $("#restock_threshold").val();
-        var supplier_status = $("#supplier_status").val() == "enabled";
-
-        var data = {
-            "product_name": product_name,
+        const data = {
             "inventory_id": inventory_id,
-            "supplier_name": supplier_name,
-            "supplier_id": supplier_id,
             "product_id": product_id,
+            "product_name": product_name,
+            "supplier_id": supplier_id,
+            "supplier_name": supplier_name,
             "supplier_status": supplier_status,
             "quantity": quantity,
             "unit_price": unit_price,
             "restock_threshold": restock_threshold,
         };
 
-        var ajax = $.ajax({
+        const ajax = $.ajax({
             type: "POST",
             url: "/inventory",
             contentType: "application/json",
             data: JSON.stringify(data),
         });
 
-        ajax.done(function(res){
+        ajax.done(function (res) {
             update_form_data(res)
             flash_message("Success")
         });
 
-        ajax.fail(function(res){
+        ajax.fail(function (res) {
             flash_message(res.responseJSON.message)
         });
     });
@@ -93,44 +91,41 @@ $(function () {
     // ****************************************
 
     $("#update-btn").click(function () {
+        const inventory_id = $("#inventory_id").val();
+        const product_id = $("#product_id").val();
+        const product_name = $("#product_name").val();
+        const supplier_name = $("#supplier_name").val();
+        const supplier_id = $("#supplier_id").val();
+        const supplier_status = $("#supplier_status").val() == "enabled";
+        const quantity = $("#quantity").val();
+        const unit_price = $("#unit_price").val();
+        const restock_threshold = $("#restock_threshold").val();
 
-
-        var product_name = $("#product_name").val();
-        var supplier_name = $("#supplier_name").val();
-        var supplier_id = $("#supplier_id").val();
-        var product_id = $("#product_id").val();
-        var inventory_id = $("#inventory_id").val();
-        var quantity = $("#quantity").val();
-        var unit_price = $("#unit_price").val();
-        var restock_threshold = $("#restock_threshold").val();
-        var supplier_status = $("#supplier_status").val() == "enabled";
-
-        var data = {
-            "product_name": product_name,
+        const data = {
             "inventory_id": inventory_id,
-            "supplier_name": supplier_name,
-            "supplier_id": supplier_id,
             "product_id": product_id,
+            "product_name": product_name,
+            "supplier_id": supplier_id,
+            "supplier_name": supplier_name,
             "supplier_status": supplier_status,
             "quantity": quantity,
             "unit_price": unit_price,
             "restock_threshold": restock_threshold,
-
         };
 
-        var ajax = $.ajax({
-                type: "PUT",
-                url: "/inventories/" + product_id,
-                contentType: "application/json",
-                data: JSON.stringify(data)
-            })
+        const ajax = $.ajax({
+            type: "PUT",
+            url: "/inventories/" + product_id,
+            contentType: "application/json",
+            data: JSON.stringify(data)
+        })
 
-        ajax.done(function(res){
+        ajax.done(function (res) {
             update_form_data(res)
             flash_message("Success")
         });
 
-        ajax.fail(function(res){
+        ajax.fail(function (res) {
             flash_message(res.responseJSON.message)
         });
 
@@ -151,13 +146,13 @@ $(function () {
             data: ''
         })
 
-        ajax.done(function(res){
+        ajax.done(function (res) {
             //alert(res.toSource())
             update_form_data(res)
             flash_message("Success")
         });
 
-        ajax.fail(function(res){
+        ajax.fail(function (res) {
             clear_form_data()
             flash_message(res.responseJSON.message)
         });
@@ -170,21 +165,21 @@ $(function () {
 
     $("#delete-btn").click(function () {
 
-        var product_id = $("#product_id").val();
+        const product_id = $("#product_id").val();
 
-        var ajax = $.ajax({
+        const ajax = $.ajax({
             type: "DELETE",
             url: "/inventory/" + product_id,
             contentType: "application/json",
             data: '',
         })
 
-        ajax.done(function(res){
+        ajax.done(function (res) {
             clear_form_data()
             flash_message("Product has been Deleted!")
         });
 
-        ajax.fail(function(res){
+        ajax.fail(function (res) {
             flash_message("Server error!")
         });
     });
@@ -203,19 +198,18 @@ $(function () {
     // ****************************************
 
     $("#search-btn").click(function () {
+        // FIXME: We dont support searching by most of these fields so those will not work. see ../../models.py to either make more search methods or determine which to remove below
+        const inventory_id = $("#inventory_id").val();
+        const product_id = $("#product_id").val();
+        const product_name = $("#product_name").val();
+        const supplier_id = $("#supplier_id").val();
+        const supplier_name = $("#supplier_name").val();
+        const supplier_status = $("#supplier_status").val() === "enabled";
 
-        
-        var product_name = $("#product_name").val();
-        var supplier_name = $("#supplier_name").val();
-        var product_id = $("#product_id").val();
-        var supplier_id = $("#product_id").val();
-        var inventory_id = $("#inventory_id").val();
-        var supplier_status = $("#supplier_status").val() == "enabled";
-
-        var queryString = ""
+        let queryString = "";
 
         if (product_name) {
-            queryString += 'name=' + product_name
+            queryString += 'product_name=' + product_name
         }
         if (product_id) {
             if (queryString.length > 0) {
@@ -223,43 +217,44 @@ $(function () {
             } else {
                 queryString += 'product_id=' + product_id
             }
-
+        }
         if (supplier_id) {
             if (queryString.length > 0) {
                 queryString += '&supplier_id=' + supplier_id
             } else {
                 queryString += 'supplier_id=' + supplier_id
             }
+        }
         if (supplier_status) {
             if (queryString.length > 0) {
-                queryString += '&enabled=' + enabled
+                queryString += '&supplier_status=' + supplier_status
             } else {
-                queryString += 'enabled=' + enabled
+                queryString += 'supplier_status=' + supplier_status
             }
-
-            if (inventory_id) {
-                if (queryString.length > 0) {
-                    queryString += '&inventory_id=' + inventory_id
-                } else {
-                    queryString += 'inventory_id=' + inventory_id
-                }
-
-            if (supplier_name) {
-                if (queryString.length > 0) {
-                    queryString += '&supplier_name=' + supplier_name
-                } else {
-                    queryString += 'supplier_name=' + supplier_name
-                }
+        }
+        if (inventory_id) {
+            if (queryString.length > 0) {
+                queryString += '&inventory_id=' + inventory_id
+            } else {
+                queryString += 'inventory_id=' + inventory_id
+            }
+        }
+        if (supplier_name) {
+            if (queryString.length > 0) {
+                queryString += '&supplier_name=' + supplier_name
+            } else {
+                queryString += 'supplier_name=' + supplier_name
+            }
         }
 
-        var ajax = $.ajax({
+        const ajax = $.ajax({
             type: "GET",
             url: "/inventory?" + queryString,
             contentType: "application/json",
             data: ''
-        })
+        });
 
-        ajax.done(function(res){
+        ajax.done(function (res) {
             //alert(res.toSource())
             $("#search_results").empty();
             $("#search_results").append('<table class="table-striped" cellpadding="10">');
@@ -269,10 +264,10 @@ $(function () {
             header += '<th style="width:40%">Category</th>'
             header += '<th style="width:10%">Available</th></tr>'
             $("#search_results").append(header);
-            var firstProduct = "";
-            for(var i = 0; i < res.length; i++) {
-                var product_id = res[i];
-                var row = "<tr><td>"+product.id+"</td><td>"+product.name+"</td><td>"+supplier.id+"</td><td>"+supplier.status+"</td></tr>"+supplier.name+"</td><td>"+inventory.id+"</td><td>";
+            let firstProduct = "";
+            for (let i = 0; i < res.length; i++) {
+                const product = res[i];
+                const row = "<tr><td>" + product.product_id + "</td><td>" + product.name + "</td><td>" + product.supplier_id + "</td><td>" + product.supplier_status + "</td></tr>" + product.supplier_name + "</td><td>" + product.id + "</td><td>";
                 $("#search_results").append(row);
                 if (i == 0) {
                     firstProduct = product;
@@ -282,17 +277,16 @@ $(function () {
             $("#search_results").append('</table>');
 
             // copy the first result to the form
-            if (firstProduct != "") {
+            if (firstProduct !== "") {
                 update_form_data(firstProduct)
             }
 
             flash_message("Success")
         });
 
-        ajax.fail(function(res){
+        ajax.fail(function (res) {
             flash_message(res.responseJSON.message)
         });
-
     });
 
 })
